98aaa7cf48053a719f0306afd904256849c1606d
Ivan <Ivan_567.92@mail.ru>
2022-08-19 18:41:38 +0300

    refactoring circle-animate animate

--- src/public/js/aplication.js
+++ src/public/js/aplication.js
@@ -84,11 +84,9 @@ setInterval(function () {
     for (let i = 0; i < $circleAllElemets.length; i++) {
         if ($circleAllElemets[i].classList.contains('circle-animate')) {
             $circleAllElemets[i].classList.remove('circle-animate');
-            if (($circleAllElemets.length - 1) === i) {
-                $circleAllElemets[0].classList.add('circle-animate');
-            } else {
+            (($circleAllElemets.length - 1) === i) ?
+                $circleAllElemets[0].classList.add('circle-animate') :
                 $circleAllElemets[i + 1].classList.add('circle-animate');
-            }
             break;
         }
     }
