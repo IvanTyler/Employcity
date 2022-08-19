cf44eb384dc426fc14cfb927d828997a7700fab7
Ivan <Ivan_567.92@mail.ru>
2022-08-19 16:18:47 +0300

    final

--- src/public/js/aplication.js
+++ src/public/js/aplication.js
@@ -1,9 +1,16 @@
+const $checkoutForm = document.forms.checkoutForm;
+
 const $selectTypeSystem = document.querySelector('.select-type-system')
 const $selectTypeSystemList = document.querySelector('.select-type-system-list')
 const $selectTypeSystemType = document.querySelector('.select-type-system__type')
 
 const $formCheckoutRange = document.querySelector('.form-checkout-range__range')
 const $formCheckoutRangeProgress = document.querySelector('.form-checkout-range-top__progress')
+const $emailForm = document.querySelector('#emailForm')
+const $nameForm = document.querySelector('#nameForm')
+
+const $circleAllElemets = document.querySelectorAll('.circle')
+
 
 function getTypesSystem() {
     let max = 999999999;
@@ -40,14 +47,49 @@ $selectTypeSystem.addEventListener('click', (e) => {
     }
 })
 
+let $currentOptionText = ''
 $selectTypeSystem.addEventListener('click', (event) => {
     if (event.target.hasAttribute('data-option')) {
         const $currentOption = event.target.closest('[data-id]')
-        const optionId = $currentOption.dataset.id
         $selectTypeSystemType.innerText = $currentOption.textContent
+        $currentOptionText = $currentOption.textContent
     }
 })
 
 $formCheckoutRange.addEventListener('input', (event) =>
     $formCheckoutRangeProgress.innerHTML = `${event.target.value} %`
 )
+
+$checkoutForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const formData = Object.fromEntries(new FormData(event.target))
+    const myformData = {
+        ...formData,
+        typeSystem: $currentOptionText,
+    }
+
+    if (!$emailForm.value) {
+        $emailForm.classList.add('error')
+    }
+    if (!$nameForm.value) {
+        $nameForm.classList.add('error')
+    } else {
+        $nameForm.classList.remove('error')
+        $emailForm.classList.remove('error')
+        console.log(myformData);
+    }
+})
+
+setInterval(() => {
+    for (let i = 0; i < $circleAllElemets.length; i++) {
+        if ($circleAllElemets[i].classList.contains('circle-animate')) {
+            $circleAllElemets[i].classList.remove('circle-animate');
+            if (($circleAllElemets.length - 1) === i) {
+                $circleAllElemets[0].classList.add('circle-animate');
+            } else {
+                $circleAllElemets[i + 1].classList.add('circle-animate');
+            }
+            break;
+        }
+    }
+}, 4000);
